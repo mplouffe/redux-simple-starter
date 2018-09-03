a422f5288e3ef703bf252e21bf74c8d08438749a
mplouffe <matheuplouffe@gmail.com>
2018-09-02 21:32:05 -0500

    Added basic functionality to the video list components and added them into the app flow.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSerach from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSerach({key: process.env.YOUTUBE_API_KEY, term: 'dota2'}, videos => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
